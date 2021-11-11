1252917277be8001e8245cc3967d219a4c54b2b0
Mike Kitzman <mdkitzman@gmail.com>
2021-11-11 07:43:18 -0700

    reorganizing

--- src/2019/day-3/index.ts
+++ src/2019/day-3/index.ts
@@ -1,6 +1,97 @@
 import fs from 'fs';
 import { min } from '../../util/arrayUtils';
 
+const main = async () => {
+  const allInput = await fs.promises.readFile('./src/2019/day-3/input', { encoding: 'utf-8'});
+  
+  const [wire1, wire2] = getWires(allInput);
+  const intersectionPoints = findIntersections(wire1, wire2);
+  
+  doPart1(wire1, wire2, intersectionPoints); // 768
+  doPart2(wire1, wire2, intersectionPoints); // 8684
+};
+
+class Point {
+  readonly x: number;
+  readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  distanceTo(p: Point): number {
+    return Math.sqrt((Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2)));
+  }
+}
+
+function doPart1(wire1: Point[], wire2: Point[], intersections: Point[]) {
+  const shortestDistance = intersections
+    .map(point => Math.abs(point.x) + Math.abs(point.y))
+    .filter(len => len > 0)
+    .reduce(min);
+  
+  console.log(`The closest intersection point is ${shortestDistance}`);
+};
+
+function doPart2(wire1: Point[], wire2: Point[], intersections: Point[]) {
+  const walkWire = (wire: Point[], point: Point): number => {
+    let distance = 0;
+    let end: Point = wire[0];
+    for(let iPoint = 0; iPoint < wire.length-1 && end !== point; iPoint++) {
+      end = pointIsOnLine(wire[iPoint], wire[iPoint+1], point)
+        ? point
+        : wire[iPoint+1];
+      distance += wire[iPoint].distanceTo(end);
+    }
+    return distance;
+  }
+
+  const shortestDistance = intersections.map(intersection => walkWire(wire1, intersection) + walkWire(wire2, intersection)).reduce(min);
+  console.log(`The shortest distance along the wires to an intersection is ${shortestDistance}`);
+};
+
+function getWires(input: string): Point[][] {
+  return input.split('\n')
+  .map(wire => wire.trim())
+  .map(wire => {
+    const points = [new Point(0,0)];
+    wire.split(',').forEach((change, index) => {
+      const lastPoint = points[index];
+      const dir = change[0];
+      const distance = parseInt(change.split('').splice(1).join(''), 10);
+      switch(dir) {
+        case 'U':
+          points.push(new Point(lastPoint.x, lastPoint.y + distance));
+          break;
+        case 'D':
+          points.push(new Point(lastPoint.x, lastPoint.y - distance));
+          break;
+        case 'R':
+          points.push(new Point(lastPoint.x + distance, lastPoint.y));
+          break;
+        case 'L':
+          points.push(new Point(lastPoint.x - distance, lastPoint.y));
+          break;
+      }
+    });
+    return points;
+  })
+}
+  
+
+function findIntersections(wire1: Point[], wire2: Point[]): Point[] {
+  const intersectionPoints: Point[] = [];
+  for (let iPoint1 = 0; iPoint1 < wire1.length - 1; iPoint1++) {
+    for (let iPoint2 = 0; iPoint2 < wire2.length - 1; iPoint2++) {
+      const intersection = computeIntersection(wire1[iPoint1], wire1[iPoint1 + 1], wire2[iPoint2], wire2[iPoint2+1]);
+      if (intersection)
+        intersectionPoints.push(intersection);
+    }
+  }
+  return intersectionPoints;
+};
+
 // https://jsfiddle.net/ferrybig/eokwL9mp/
 function computeH(a: Point, b: Point, c: Point, d: Point): number {
   // E = B-A = ( Bx-Ax, By-Ay )
@@ -42,95 +133,5 @@ function pointIsOnLine(a:Point, b:Point, q:Point):boolean {
     return q.y === a.y && isBetween(a.x, b.x, q.x);
   return false;
 }
-
-class Point {
-  readonly x: number;
-  readonly y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  distanceTo(p: Point): number {
-    return Math.sqrt((Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2)));
-  }
-}
-
-const getWires = (input: string): Point[][] => input
-  .split('\n')
-  .map(wire => wire.trim())
-  .map(wire => {
-    const points = [new Point(0,0)];
-    wire.split(',').forEach((change, index) => {
-      const lastPoint = points[index];
-      const dir = change[0];
-      const distance = parseInt(change.split('').splice(1).join(''), 10);
-      switch(dir) {
-        case 'U':
-          points.push(new Point(lastPoint.x, lastPoint.y + distance));
-          break;
-        case 'D':
-          points.push(new Point(lastPoint.x, lastPoint.y - distance));
-          break;
-        case 'R':
-          points.push(new Point(lastPoint.x + distance, lastPoint.y));
-          break;
-        case 'L':
-          points.push(new Point(lastPoint.x - distance, lastPoint.y));
-          break;
-      }
-    });
-    return points;
-  });
-
-const findIntersections = (wire1: Point[], wire2: Point[]): Point[] => {
-  const intersectionPoints: Point[] = [];
-  for (let iPoint1 = 0; iPoint1 < wire1.length - 1; iPoint1++) {
-    for (let iPoint2 = 0; iPoint2 < wire2.length - 1; iPoint2++) {
-      const intersection = computeIntersection(wire1[iPoint1], wire1[iPoint1 + 1], wire2[iPoint2], wire2[iPoint2+1]);
-      if (intersection)
-        intersectionPoints.push(intersection);
-    }
-  }
-  return intersectionPoints;
-};
-
-
-const doPart1 = (wire1: Point[], wire2: Point[], intersections: Point[]) => {
-  const shortestDistance = intersections
-    .map(point => Math.abs(point.x) + Math.abs(point.y))
-    .filter(len => len > 0)
-    .reduce(min);
-  
-  console.log(`The closest intersection point is ${shortestDistance}`);
-};
-
-const doPart2 = (wire1: Point[], wire2: Point[], intersections: Point[]) => {
-  const walkWire = (wire: Point[], point: Point): number => {
-    let distance = 0;
-    let end: Point = wire[0];
-    for(let iPoint = 0; iPoint < wire.length-1 && end !== point; iPoint++) {
-      end = pointIsOnLine(wire[iPoint], wire[iPoint+1], point)
-        ? point
-        : wire[iPoint+1];
-      distance += wire[iPoint].distanceTo(end);
-    }
-    return distance;
-  }
-
-  const shortestDistance = intersections.map(intersection => walkWire(wire1, intersection) + walkWire(wire2, intersection)).reduce(min);
-  console.log(`The shortest distance along the wires to an intersection is ${shortestDistance}`);
-};
-
-const main = async () => {
-  const allInput = await fs.promises.readFile('./src/2019/day-3/input', { encoding: 'utf-8'});
-  
-  const [wire1, wire2] = getWires(allInput);
-  const intersectionPoints = findIntersections(wire1, wire2);
-  
-  doPart1(wire1, wire2, intersectionPoints); // 768
-  doPart2(wire1, wire2, intersectionPoints); // 8684
-};
 
 main();
