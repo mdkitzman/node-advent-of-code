aed4dc7b7abb6f81453add1f79568f1a42085fa6
Mike Kitzman <mdkitzman@gmail.com>
2021-12-01 14:26:44 -0700

    2021 Day 1

---
+++ src/2021/day-1/index.ts
@@ -0,0 +1,39 @@
+import fs  from 'fs';
+import { sum } from '../../util/arrayUtils';
+
+const main = async () => {
+  const allInput = await fs.promises.readFile(`${__dirname}/input`, { encoding: 'utf-8'});
+  doPart1(allInput); // 1665
+  doPart2(allInput); // 1702
+};
+
+function doPart1(input: string) {
+  let count = 0;
+  const vals = input
+    .split('\n')
+    .map(line => parseInt(line, 10))
+  for (let i =0; i < vals.length -1; i++) {
+    if (vals[i] < vals[i+1])
+      count++;
+  }
+  console.log(count)
+};
+
+function doPart2(input: string) {
+  let count = 0;
+  const windowVals: number[] = [];
+  const vals = input.split('\n').map(line => parseInt(line, 10))
+
+  for (let i =0; i < vals.length - 2; i++) {
+    windowVals.push(vals[i] + vals[i+1] + vals[i+2]);
+  }
+
+  for (let i = 0; i < windowVals.length - 1; i++) {
+    if (windowVals[i] < windowVals[i+1])
+      count++;
+  }
+  
+  console.log(count);
+};
+
+main();
