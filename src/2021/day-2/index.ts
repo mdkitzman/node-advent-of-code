37e4484c93aea4fd79f7984ba7dcbe536be66c99
Mike Kitzman <mdkitzman@gmail.com>
2021-12-01 22:14:29 -0700

    2021 Day 2

---
+++ src/2021/day-2/index.ts
@@ -0,0 +1,54 @@
+import fs  from 'fs';
+import { Point2D } from '../../util/point';
+
+const main = async () => {
+  const allInput = await fs.promises.readFile(`${__dirname}/input`, { encoding: 'utf-8'});
+  doPart1(allInput); // 1654760
+  doPart2(allInput); // 1956047400
+};
+
+function doPart1(input: string) {
+  const point = new Point2D(0,0);
+  input.split('\n')
+    .forEach(line => {
+      const [dir, valStr] = line.split(' ');
+      const val = parseInt(valStr);
+      switch (dir) {
+        case "forward":
+          point.x += val;
+          break;
+        case "down":
+          point.y += val;
+          break;
+        case "up":
+          point.y -= val;
+          break;
+      }
+    });
+    console.log(`Ended up at point ${point.x}, ${point.y} with multipled values of ${point.x * point.y}`);
+};
+
+function doPart2(input: string) {
+  const point = new Point2D(0,0);
+  let aim = 0;
+  input.split('\n')
+    .forEach(line => {
+      const [dir, valStr] = line.split(' ');
+      const val = parseInt(valStr);
+      switch (dir) {
+        case "forward":
+          point.y += val * aim;
+          point.x += val;
+          break;
+        case "down":
+          aim += val;
+          break;
+        case "up":
+          aim -= val;
+          break;
+      }
+    });
+    console.log(`Ended up at point ${point.x}, ${point.y} with multipled values of ${point.x * point.y}`);
+};
+
+main();
