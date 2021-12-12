baf1fbd28de1491d3f1098f110677d35854f28b4
Mike Kitzman <mdkitzman@gmail.com>
2021-12-11 18:57:00 -0700

    2021 day 11

---
+++ src/2021/day-11/index.ts
@@ -0,0 +1,94 @@
+import fs  from 'fs';
+import { Grid } from '../../util/grid';
+import { Point2D } from '../../util/point';
+
+const main = async () => {
+  const allInput = await fs.promises.readFile(`${__dirname}/input`, { encoding: 'utf-8'});
+  doPart1(allInput); // 1683
+  doPart2(allInput); // 788
+};
+
+function doPart1(input: string) {
+  const grid = new Grid<number>();
+  
+  input
+    .split('\n')
+      .forEach((line, y) => line
+        .split('')
+        .forEach((val, x) => grid.set(new Point2D(x,y), Number(val))));
+
+  const numberOfFlashes = simulateFlashes(grid, 100);
+
+  console.log(`There were ${numberOfFlashes} flashes`);
+};
+
+function doPart2(input: string) {
+  const grid = new Grid<number>();
+  
+  input
+    .split('\n')
+      .forEach((line, y) => line
+        .split('')
+        .forEach((val, x) => grid.set(new Point2D(x,y), Number(val))));
+
+  let iteration = 0;
+  let flashCount = 0;
+  do {
+    iteration++;
+    flashCount = simulateFlashes(grid, 1);
+    if (flashCount === grid.data.size)
+      break;
+  } while (true);
+  
+  console.log(`It took ${iteration} iterations to synchronize`);
+};
+
+const neighborArray = [
+  [-1, -1], [0, -1], [+1, -1],
+  [-1,  0],          [+1,  0],
+  [-1, +1], [0, +1], [+1, +1],
+].map(([x,y]) => new Point2D(x,y));
+
+function simulateFlashes(grid: Grid<number>, steps: number):number {
+  let flashCount = 0;
+  for(let iStep = 0; iStep < steps; iStep++) {
+    // increase all by 1
+    grid.forEach((key, value) => grid.set(key, value+1));
+
+    const seen: Point2D[] = [];
+    let lastSeenCount = 0;
+    do {
+      lastSeenCount = seen.length;
+      grid.forEach((key, value) => {
+        if(value <= 9)
+          return;
+        if(seen.some(value => value.equals(key)))
+          return;
+        seen.push(key);
+        // any > 9 flash
+        flashCount++;
+        // increase neighbors by 1
+        neighborArray
+          .map(p => new Point2D(key.x + p.x, key.y + p.y))
+          .forEach(neighborKey => {
+            const val = grid.get(neighborKey);
+            if (val !== undefined)
+              grid.set(neighborKey, val+1);           
+          });
+      });
+    } while(lastSeenCount < seen.length)
+
+    // set the flashers back to 0
+    grid.forEach((key, value) => {
+      if(value > 9)
+        grid.set(key, 0);
+    });
+
+    //grid.print('low',String);
+    //console.log('');
+  }
+
+  return flashCount;
+}
+
+main();
---
+++ src/util/grid.ts
@@ -0,0 +1,61 @@
+import { range } from "lodash";
+import { Point2D } from "./point";
+
+export class Grid<T> {
+  public readonly data = new Map<string, T>();
+  
+  constructor(){}
+
+  static toPoint(key:string) {
+    const [x, y] = key.split(',').map(Number);
+    return new Point2D(x,y);
+  }
+
+  static toKey(point: Point2D): string {
+    return `${point.x},${point.y}`;
+  }
+
+  public get(point: Point2D): T | undefined {
+    const key = Grid.toKey(point);
+    return this.data.get(key)!;
+  }
+
+  public set(point: Point2D, value: T): void {
+    const key = Grid.toKey(point);
+    this.data.set(key, value);
+  }
+
+  public forEach(action:(key: Point2D, value: T)=>void) {
+    Array
+      .from(this.data.entries())
+      .forEach(([key, value]) => 
+        action(Grid.toPoint(key), value as T)
+      );
+  }
+
+  public print(verticalStart: 'high'|'low' = 'high', printValue: (value?: T)=>string) {
+    const points = Array
+      .from(this.data.keys())
+      .map(Grid.toPoint);
+    
+    const left   = points.map(p => p.x).reduce((x1, x2) => x1 <= x2 ? x1 : x2);
+    const right  = points.map(p => p.x).reduce((x1, x2) => x1 >= x2 ? x1 : x2);
+    
+    const top    = points.map(p => p.y).reduce((y1, y2) => y1 >= y2 ? y1 : y2);
+    const bottom = points.map(p => p.y).reduce((y1, y2) => y1 <= y2 ? y1 : y2);
+
+    const yRange:number[] = verticalStart === 'high'
+      ? range(top, bottom)
+      : range(bottom, top);
+    
+    yRange.forEach(y => {
+      let row = '';
+      for (let x = left; x <= right; x++) {
+        const value:T|undefined = this.data.get(`${x},${y}`);
+        row += printValue(value);
+      }
+      console.log(row);
+      row = '';
+    });
+  }
+}
