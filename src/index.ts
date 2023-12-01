f75a8a5dd289888c961761353b20b02c77e7e824
Mike Kitzman <mdkitzman@gmail.com>
2023-12-01 01:00:02 -0600

    Help tweaks

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import chalk from 'chalk';
 import figlet from 'figlet';
 import * as APPS from './applications'
 
-const pkg = require("../package.json"); // from local lib
-const banner = chalk.red(figlet.textSync(pkg.name, { horizontalLayout: 'full', font: 'Slant' }));
+const pkg = require(`${__dirname}/../package.json`); // from local lib
+const banner = chalk.red(figlet.textSync(pkg.name, { horizontalLayout: 'full', font: 'Rectangles' }));
 const program = new Command();
 program.version(pkg.version);
 // Pass the program to each application and setup the command and arguments
