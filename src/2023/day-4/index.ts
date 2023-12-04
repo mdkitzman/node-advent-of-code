b35846ecd60b25184cedccf84579786db501b6c6
Mike Kitzman <mdkitzman@gmail.com>
2023-12-04 07:40:53 -0600

    2023 Day 4 tweaks

--- src/2023/day-4/index.ts
+++ src/2023/day-4/index.ts
@@ -8,37 +8,34 @@ const main = async () => {
   doPart2(allInput);
 };
 
+const findMatches = ([winners, myNumbers]: string[][]) => intersection(winners, myNumbers)
 const getScratchCards = (input: string) => {
   return input
     .split("\n")
-    .map(line => line.split(":").map(s => s.trim()))
-    .map(([,numbers]) => numbers.split("|").map(s => s.trim()))
+    .map(line => /Card\s+\d+: ([\s\d]+) \| ([\s\d]+)/.exec(line)!)
+    .map(([,leftNumbers,rightNumbers]) => ([leftNumbers.trim().split(/\s+/), rightNumbers.trim().split(/\s+/)]))
+    .map(findMatches)
 }
 
-const calculatePoints = ([winners, myNumbers]: string[]): number => {
-  const matches = intersection(winners.split(/\s+/), myNumbers.split(' '));
-  return matches.length === 0
-    ? 0
-    : Math.pow(2, matches.length - 1)
-}
 
 function doPart1(input: string) {
   const totalPoints = getScratchCards(input)
-    .map(calculatePoints)    
+    .filter(matches => matches.length > 0)
+    .map(function calculatePoints(matches) {
+      return Math.pow(2, matches.length - 1)
+    })    
     .reduce(sum);
     
   console.log(totalPoints);
 };
 
 function doPart2(input: string) {
-  const scratchCards = getScratchCards(input)
-  const cardCounter = new Array(scratchCards.length).fill(1);
-
-  scratchCards.forEach(([winners, myNumbers], iCard, arr) => {
-    const matches = intersection(winners.split(/\s+/), myNumbers.split(' '));
-    for(let i = 0; i<matches.length; i++)
-      cardCounter[i+iCard+1] += cardCounter[iCard];
-  });
+  const cardCounter:number[] = new Array(input.split('\n').length).fill(1);
+  getScratchCards(input)
+    .forEach((matches, iCard) => {
+      for(let i = 0; i<matches.length; i++)
+        cardCounter[i+iCard+1] += cardCounter[iCard];  
+    })
 
   console.log(cardCounter.reduce(sum))
 };
