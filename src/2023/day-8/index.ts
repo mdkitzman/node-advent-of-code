bea4983011c4213e7c2a6cd9c3f95600de4452f7
Mike Kitzman <mdkitzman@gmail.com>
2023-12-08 20:26:05 -0600

    2023 Day 8

---
+++ src/2023/day-8/index.ts
@@ -0,0 +1,56 @@
+import fs  from 'fs';
+import timeFn from '../../util/timeFn';
+import { infiniteLoop } from '../../util/arrayUtils';
+import { lcm } from '../../util/numberUtils';
+type NodeMap = Map<string, [string,string]>;
+
+const timedPart1 = timeFn(doPart1)
+const timedPart2 = timeFn(doPart2);
+
+const main = async () => {
+  const allInput = await fs.promises.readFile(`${__dirname}/input`, { encoding: 'utf-8'});
+  
+  timedPart1(allInput);
+  timedPart2(allInput);
+};
+
+function doPart1(input: string) {
+  const [instructions, nodeMap] = getNodesAndDirections(input);
+  
+  let counter = walkNodes(instructions, "AAA", nodeMap);
+  console.log(counter); // 18113
+};
+
+function doPart2(input: string) {
+  const [instructions, nodeMap] = getNodesAndDirections(input);
+
+  const counter = [...nodeMap.keys()]
+    .filter(node => node.endsWith("A"))
+    .map(start => walkNodes(instructions, start, nodeMap))
+    .reduce(lcm);
+  console.log(counter); // 12315788159977
+};
+
+function getNodesAndDirections(input:string): [string, NodeMap] {
+  const nodeMap = new Map<string, [string, string]>();
+  const [instructions, nodeLines] = input.split("\n\n");
+  nodeLines
+    .split("\n")
+    .forEach(line => {
+      const [,key, left, right] = /(\S+) = \((\S+), (\S+)\)/.exec(line)!;
+      nodeMap.set(key, [left, right]);
+    });
+  return [instructions, nodeMap];
+}
+
+function walkNodes(instructions:string, node:string, nodeMap: Map<string, [string, string]>): number {
+  let counter = 0;
+  for(const direction of infiniteLoop(instructions.split(""))) {
+    node = nodeMap.get(node)![direction === "L" ? 0 : 1];
+    counter++;
+    if (node.endsWith("Z")) break;
+  }
+  return counter;
+}
+
+main();
